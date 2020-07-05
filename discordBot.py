e1d061d90f42d8d512fbd71fe7cd525e4be9c65d
jay9989 <33640800+jay9989@users.noreply.github.com>
2020-07-05 18:16:40 -0500

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -141,7 +141,7 @@ async def list(ctx):
 
 @client.command()
 async def invite(ctx):
-	await ctx.message.channel.send("https://discord.gg/togethearn")
+	await ctx.message.channel.send("https://discord.gg/XGzyksp")
 
 @client.command()
 async def clear(ctx):
