95db3f0709dca9a96bd65d2c453fcc55e4107b30
UTC Jay <jxh869@mocs.utc.edu>
2020-07-21 08:14:49 -0500

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -1,4 +1,7 @@
+import asyncio
+import json
 import random
+import re
 import string
 import os
 import discord
@@ -14,47 +17,117 @@ ref = ["j.moomoo.com", "share.firstrade.com", "act.webull.com", "dough.com/refer
 client.remove_command('help')
 
 
+
 @client.event
 async def on_ready():
     print('Bot is ready!')
     global channel
+    client.loop.create_task(clearUsers())
+
+
 
 
 @client.event
 async def on_message(message):
+
     await client.process_commands(message)
+
     if toggle == False:
 
-        channel = os.environ['CHANNEL']
-        channel = int(channel)
+        refWarningChannel = os.environ['REF_CHANNEL']
+        refWarningChannel = int(refWarningChannel)
 
-        channel = client.get_channel(channel)
-        messageBackup = message
+        refWarningChannel = client.get_channel(refWarningChannel)
         for x in range(0, len(ref)):
             if ref[x] in message.content:
                 await message.delete()
                 print(message.author)
                 await message.author.send(
-                    "Hello! We've noticed you sent a referral link and while we appreciate you trying to help others, referal links are reserved for those who have earned the right. Reach out to us for our assistance with how to get referalls.  If you have any questions or would like to help, don\'t hesitate to message an Admin! Thank you for being a part of Learn and Earn!")
-                await channel.send(
+                    "Hello! We've noticed you sent a referral link and while we appreciate you trying to help others, referal links are reserved for those who have earned the right. Reach out to us for our assistance with how to get referalls.  If you have any questions or would like to help, don\'t hesitate to message an Admin! Thank you for being a part of Learn & Earn!")
+                await refWarningChannel.send(
                     f"{message.author.mention} has tried to send a referral link.")
-
 
     donateChannel = os.environ['DONATE_CHANNEL']
     donateChannel = int(donateChannel)
 
     if message.channel.id == donateChannel:
-
         await client.process_commands(message)
-        time.sleep(60)
+        await asyncio.sleep(60)
         await message.delete()
+
+@client.event
+async def on_member_join(member):
+
+    starthere = client.get_channel(os.environ['START_HERE'])
+    userAgreementchannel = client.get_channel(os.environ['USER_AGREEMENT'])
+
+    server = starthere.guild
+    role = discord.utils.find(lambda r: r.name == 'New User', server.roles)
+    await member.add_roles(role)
+
+    await starthere.send(
+        f"Welcome to Learn and Earn {member.mention}! Press the green checkbox in the {userAgreementchannel.mention} channel to get access to the full server.")
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == os.environ['MESSAGE_ID']:
+        print(payload.emoji.name)
+        # Find a role corresponding to the Emoji name.
+        guild_id = payload.guild_id
+
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        roleAdd = discord.utils.find(lambda r: r.name == 'Community Members', guild.roles)
+        roleRemove = discord.utils.find(lambda r: r.name == 'New User', guild.roles)
+
+        print(payload.emoji.name)
+        if(payload.emoji.name == '✅'):
+            print("inhere")
+
+            role = discord.utils.get(guild.roles, name="Community Members")
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            await member.add_roles(roleAdd)
+            await member.remove_roles(roleRemove)
+            print("done")
+
+
+
+
+@client.event
+async def clearUsers():
+    await client.wait_until_ready()
+    while True:
+        startchannel = client.get_channel(os.environ['START_HERE'])
+
+        server = startchannel.guild
+        role = discord.utils.find(lambda r: r.name == 'New User', server.roles)
+
+        for member in server.members:
+
+            if role in member.roles:
+
+                print(f"Removed: {member}")
+                members = str(member)
+                memberNameTemp = re.match(r"[^#]+", members)
+                memberName = memberNameTemp.group()
+                await member.send(
+                    f"Hi, {memberName} we noticed you joined our group but have not gotten full access to all of our channels. To get full access please go to the user agreement channel and click the green check mark one time to have full access. To make this process easier for you I have removed you and you can use the attached invite to rejoin the group and get properly verified. Hope to see you soon, {memberName}.\n Invite link: https://discord.gg/XGzyksp")
+                # await server.kick(member, reason="User did not verify")
+
+        await asyncio.sleep(3600)
+
+
 
 
 @client.command()
 async def enable(ctx):
     """ -- Enable referral auto delete."""
 
-    role = discord.utils.get(ctx.guild.roles, name="RBAs")
+    role = discord.utils.get(ctx.guild.roles, name="Admins")
     if role in ctx.author.roles:
         global toggle
         toggle = not toggle
@@ -67,90 +140,14 @@ async def enable(ctx):
 @client.command()
 async def roll(ctx, max: int):
     """ -- Roll number between 1 and {input}"""
-
-
-
-    # Get the server channel to send the "user rolled # out of #" message to
-    channelID = os.environ['ROLL_CHHANNEL']
-    channelID = int(channelID)
-    roll_channel = client.get_channel(channelID)
-    # Get the random roll number from 1 to the number that was inputed
-    rolled = random.randint(1, max)
-
-    # If the user that is rolling does not have their name in the list
-    if ctx.message.author.name not in NAMES:
-
-        # Send the "User rolled # out of #" message to the admin channel
-        await roll_channel.send(f"{ctx.message.author.mention} has rolled a " + str(rolled) + " out of " + str(max))
-
-        # Add the user's name to the list so they can not reroll
-        NAMES.append(ctx.message.author.name)
-
-    else:
-
-        # If the name already exists in the list, send this message to the user
-        await ctx.message.author.send(
-            "Sorry, but you've already rolled in the Togethearn raffle. If there has been a mistake, please contact the Togethearn staff.")
-
-
-@client.command()
-async def rem(ctx, name: str):
-    """ -- Remove user from rolled users."""
-    # This will give us the User ID without "<", ">", and "@"
-    name = name.replace("<", "")
-    name = name.replace(">", "")
-    name = name.replace("@", "")
-
-    # This gets the role from the server
-    role = discord.utils.get(ctx.guild.roles, name="RBAs")
-
-    # This returns the user's name
-    user = client.get_user(int(name))
-    username = user.name
-
-    # If the role of the user matches the required role
+    role = discord.utils.get(ctx.guild.roles, name="Admins")
     if role in ctx.author.roles:
+        rolled = random.randint(1, max)
+        await ctx.channel.send(f"You have rolled a " + str(rolled) + " out of " + str(max))
 
-        # If the username exists in the list
-        if username in NAMES:
-
-            # Remove the name from the list
-            NAMES.remove(username)
-
-            # For debugging purposes
-            print(NAMES)
-
-        else:
-
-            # If the user does not exist on the list, send this message
-            await ctx.author.send("The user " + username + " has not yet rolled.")
-
-
-
-
-
-
-@client.command()
-async def list(ctx):
-    """ -- Get list of all users that have rolled."""
-
-    role = discord.utils.get(ctx.guild.roles, name="RBAs")
-
-    if role in ctx.author.roles:
-        await ctx.send(NAMES)
 
 @client.command()
 async def invite(ctx):
-	await ctx.message.channel.send("https://discord.gg/4EdCTGh")
-
-@client.command()
-async def clear(ctx):
-    """ -- Clear list of users that have rolled."""
-
-    role = discord.utils.get(ctx.guild.roles, name="RBAs")
-
-    if role in ctx.author.roles:
-        NAMES.clear()
-        await ctx.author.send("The list has been cleared.")
+    await ctx.message.channel.send("https://discord.gg/4EdCTGh")
 
 client.run(os.environ['TOKEN'])
